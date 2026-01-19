0cf4df147e36dfc01ea2c1c724233b9e68acc904
Roy Zhao <royzhao2004@outlook.com>
2026-01-19 13:20:06 -0600

    feat: implement user API client and DTO for fetching user data

---
+++ src/features/auth/api/backend/dto.ts
@@ -0,0 +1,7 @@
+import { paths } from "../../../../app/DTOTypes";
+
+export const USERS_ME_API_PATH = "/api/v1/users/me";
+
+
+export type UserResponse =
+  paths[typeof USERS_ME_API_PATH]["post"]["responses"]["200"]["content"]["application/json"];
