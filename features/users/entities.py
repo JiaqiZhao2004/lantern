3f4fef4844c6a8bc00e7f33481569c73eb265237
Roy Zhao <royzhao2004@outlook.com>
2026-01-19 10:50:09 -0600

    refactored repo structure

--- features/users/entities.py
+++ features/users/entities.py
@@ -1,12 +1,12 @@
 # models.py
 from sqlalchemy import Column, Integer, String
-from .database import Base
+from ...db.database import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     firebase_uid = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     name = Column(String)
