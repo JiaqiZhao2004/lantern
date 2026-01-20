300e0080ee4c7ee902c35657b335a24e96ec3939
Roy Zhao <royzhao2004@outlook.com>
2026-01-19 21:33:29 -0600

    connect /users/me endpoint

--- src/features/auth/state/AuthContext.tsx
+++ src/features/auth/state/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext } from "react";
 import { AuthStateAction, AuthState, initialAuthState } from "./AuthModels";
 
 interface IAuthContext extends AuthState {
--- src/features/dashboard/pages/DashboardPage.tsx
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -1,7 +1,44 @@
+import { useCallback, useContext, useEffect } from "react";
 import Header from "../../../Components/Header";
 import PlaidLinkPage from "../../plaid/pages/PlaidLinkPage";
+import { DashboardProvider } from "../state/DashboardProvider";
+import { DashboardContext } from "../state/DashboardContext";
+import { get_or_create_me } from "../api/backend/client";
+import { AuthContext } from "../../auth/state/AuthContext";
 
 export default function DashboardPage() {
+  return (
+    <DashboardProvider>
+      <Content></Content>
+    </DashboardProvider>
+  );
+}
+
+function Content() {
+  const { user, dispatch } = useContext(DashboardContext);
+  const { isAuthenticated } = useContext(AuthContext);
+
+  const getOrCreateMe = useCallback(async () => {
+    if (!isAuthenticated) {
+      return;
+    }
+    const data = await get_or_create_me();
+    dispatch({
+      type: "SET_STATE",
+      state: {
+        user: { email: data.email, name: data.name },
+      },
+    });
+  }, [isAuthenticated, dispatch]);
+
+  useEffect(() => {
+    getOrCreateMe();
+  }, [getOrCreateMe]);
+
+  useEffect(() => {
+    console.log("user changed:", user);
+  }, [user]);
+
   return (
     <div>
       <Header></Header>
