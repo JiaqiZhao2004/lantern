7cdfa7c7c347df9686e09371d99314a508253851
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2026-01-25 22:45:20 -0600

    moved init_db

--- init_db.py
+++ init_db.py
@@ -1,8 +1,9 @@
 # scripts/init_db.py
 from services.db.database import engine, Base
-
+# ImportError: attempted relative import with no known parent package
 # IMPORTANT: import entities so they register
 import features.users.entities
+import features.plaid.entities
 
 print("Creating tables...")
 Base.metadata.create_all(bind=engine)
