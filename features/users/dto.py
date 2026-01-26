166c1a5d4b492889ce0442bf55a05eb0c8173399
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2026-01-25 22:45:55 -0600

    use UUID for user id

--- features/users/dto.py
+++ features/users/dto.py
@@ -2,10 +2,11 @@
 # Data Transfer Objects (DTOs) for API requests and responses
 
 from pydantic import BaseModel, EmailStr
+from uuid import UUID
 
 
 class UserResponseDTO(BaseModel):
-    id: int
+    id: UUID
     email: EmailStr
     firebase_uid: str
     name: str | None = None
