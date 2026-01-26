742684995fb24e7f9d87f4e7201d427aaf6fe8b2
Roy Zhao <royzhao2004@outlook.com>
2026-01-25 22:45:55 -0600

    use UUID for user id

--- features/users/entities.py
+++ features/users/entities.py
@@ -1,13 +1,21 @@
 # features/users/entities.py
 # Persistent entities for Database ORM mapping
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
 from services.db import Base
+from uuid6 import uuid7
+import uuid
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid7,
+    )
     firebase_uid = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     name = Column(String)
