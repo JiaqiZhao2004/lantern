838a88066bc07c66c170fca47b17717288b83c25
I-jzhao_CCCIS <I-jzhao@cccis.com>
2026-03-01 11:07:02 -0600

    simple connectionsPanel

--- src/features/dashboard/api/backend/client.ts
+++ src/features/dashboard/api/backend/client.ts
@@ -1,7 +1,12 @@
 import axiosClient from "../../../../core/axiosConfig";
-import { UserResponse, USERS_ME_API_PATH } from "./dto";
+import { UserResponse, USERS_ME_API_PATH, GetItemsResponse, GET_ITEMS_API_PATH } from "./dto";
 
 export async function get_or_create_me() {
   const res = await axiosClient.post<UserResponse>(USERS_ME_API_PATH);
+  return res.data;
+}
+
+export async function get_items(): Promise<GetItemsResponse> {
+  const res = await axiosClient.get<GetItemsResponse>(GET_ITEMS_API_PATH);
   return res.data;
 }
