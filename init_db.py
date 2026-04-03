a10973aaccb6cba6b9c05fb419e9bb2146d45c31
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2026-04-02 22:42:35 -0500

    move users to app/users

--- init_db.py
+++ init_db.py
@@ -2,7 +2,7 @@
 from services.db.database import engine, Base
 # ImportError: attempted relative import with no known parent package
 # IMPORTANT: import entities so they register
-import src.users.entities
+import src.app.users.entities
 import src.plaid.entities
 
 print("Creating tables...")
