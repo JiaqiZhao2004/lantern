5e1cafe1e60ba60ccb426ac3393f6d2bdcb1665c
Roy Zhao <royzhao2004@outlook.com>
2026-04-02 23:37:35 -0500

    refactoring dependencies

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,7 +1,27 @@
 from fastapi import Depends
+from sqlalchemy.orm import Session
 
 from src.app.user.repository import UserRepository
 from src.app.user.service import UserService
+
+from src.infrastructure import get_db, get_firebase_claims
+from dataclasses import dataclass
+
+
+@dataclass
+class RequestContext:
+    db: Session
+    claims: dict
+
+
+def get_request_context(
+    db: Session = Depends(get_db),
+    claims: dict = Depends(get_firebase_claims),
+) -> RequestContext:
+    return RequestContext(
+        db=db,
+        claims=claims,
+    )
 
 
 def get_user_repository() -> UserRepository:
--- src/api/routes/users.py
+++ src/api/routes/users.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
 
 from src.app.user import dto
-from src.infrastructure import get_db, get_firebase_claims
-from ..dependencies import get_user_service
+from ..dependencies import (
+    get_user_service,
+    get_request_context,
+    RequestContext,
+)
 from src.app.user.service import UserService
 
 router = APIRouter(prefix="/api/v1/users", tags=["users"])
@@ -11,11 +13,10 @@ router = APIRouter(prefix="/api/v1/users", tags=["users"])
 
 @router.post("/me", response_model=dto.UserResponseDTO)
 def get_or_create_me(
-    db: Session = Depends(get_db),
-    claims: dict = Depends(get_firebase_claims),
+    ctx: RequestContext = Depends(get_request_context),
     user_service: UserService = Depends(get_user_service),
 ):
     try:
-        return user_service.get_or_create_me(db=db, claims=claims)
+        return user_service.get_or_create_me(db=ctx.db, claims=ctx.claims)
     except Exception as e:
         return HTTPException(500, e)
