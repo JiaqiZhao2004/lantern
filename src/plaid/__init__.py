61da3de9a956a3c4825537813d7b885cf5cc6cbd
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2026-04-25 23:36:31 -0500

    add DI support for transactions repo & service and syncjobs exec service

--- src/plaid/__init__.py
+++ src/plaid/__init__.py
@@ -2,3 +2,5 @@ from .items.repository import PlaidItemRepository
 from .items.service import PlaidItemService
 from .accounts.repository import PlaidAccountRepository
 from .accounts.service import PlaidAccountService
+from .transactions.repository import TransactionRepository
+from .transactions.service import TransactionService
